125958bae88e0be35800c6c252ab4a70391b5ecc
gullytech <gullytech@gmail.com>
2014-10-30 07:24:33 +1100

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -9,13 +9,12 @@
     // your code here
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
-    [character flap];
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
-
+    [character flap];
 }
 
 // put new methods here
